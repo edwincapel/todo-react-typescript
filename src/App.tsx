3b11f54f0ee967c0377eb88cf038bf3d8c392d1b
Edwin Capel <edwinrc1026@gmail.com>
2019-06-25 15:45:25 +0800

    user can add item

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-interface ITod {
+import {
+  Container,
+  Row,
+  Col,
+  Button, 
+  Form, 
+  FormGroup, 
+  Label, 
+  Input, 
+  FormText
+} from 'reactstrap';
+
+
+interface ITodo {
   text:string,
   complete: boolean
 }
 
 const App: React.FC = () => {
+  const [value, setValue] = useState<string>('')
+  const [todo, setTodo] = useState<ITodo[]>([])
 
   return (
-
+    <>
+      <Container fluid className="h-100">
+        <Row className="h-100">
+          <Col md="6" className="d-flex justify-content-center align-items-center">
+            <Form className="w-75">
+              <FormGroup>
+                <Label for="newTodo">New Todo</Label>
+                <Input type="text" name="newTodo" id="newTodo" placeholder="add New Item here" />
+              </FormGroup>
+              <Button>Add new item</Button>
+            </Form>
+          </Col>
+          <Col md="6" className="border-left">
+          
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
